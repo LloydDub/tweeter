498ccdf6258dbae8cee44cbe4c3b46a196ae0819
Lloyd Worth <lloydrww@gmail.com>
2021-09-25 19:28:13 +0000

    formating and editing post code review

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,81 +1,57 @@
-
-
 $(document).ready(function () {
-$("#error1").hide();
-$("#error2").hide();
-loadTweets();
-     $('#tweetForm').on('submit', onSubmit);
-    
-    // renderTweets(data)
-  })
+  $("#error1").hide();
+  $("#error2").hide();
+  loadTweets();
+  $("#tweetForm").on("submit", onSubmit);
+});
 
- 
-  const onSubmit = function (event) {
-    event.preventDefault();
-    
-    $("#error1").hide();
-    $("#error2").hide();
+const onSubmit = function (event) {
+  event.preventDefault();
+  $("#error1").hide();
+  $("#error2").hide();
 
+  const data = $(this).serialize();
 
- 
-    console.log("good job on pressing a button!")
-    
-    const data = $(this).serialize();
-    
-
-
-    if (data.length > 145) {
-      $("#error2").slideDown("slow")
-      return false;
-    } else if (data.length < 6) {
-      $("#error1").slideDown("slow")
-      return false
-
-    }
-    $.post("/tweets", data)
-      .then(()=>{
-        loadTweets()
-      });
-      
+  if (data.length > 145) {
+    $("#error2").slideDown("slow");
+    return;
+  } else if (data.length < 6) {
+    $("#error1").slideDown("slow");
+    return;
   }
-  
-  const renderTweets = function (tweets) {
-    tweets.forEach((tweet) => {
-      console.log("look at me", tweet)
-      const $tempData = createTweetElement(tweet);   
-      $('#tweets-container').prepend($tempData);
-     
-      
-    });
-  
-  };
-  
-    function loadTweets() {
-      $('#tweets-container').empty()  // empties the container so there is not duplicates
-      
-      console.log('loadtweets' )
-      $.ajax({
-        url: "/tweets",
-        method: "GET",
-        // data: data,
-        dataType: "json",
-        success:  (tweet) => {
-          console.log('succsess', tweet)
-          renderTweets(tweet);
-          
-        }
-      });
-    }
-    
-  
-  
-  const createTweetElement = function (tweet) {
-    const name = tweet.user.name;
-    const avatars = tweet.user.avatars;
-    const handle = tweet.user.handle;
-    // const content = tweet.content.text;
-    const created = moment(tweet.created_at).fromNow()
-    let $tweet = `<section class="kuro">
+
+  $.post("/tweets", data).then(() => {
+    loadTweets();
+  });
+};
+
+const renderTweets = function (tweets) {
+  tweets.forEach((tweet) => {
+    const $tempData = createTweetElement(tweet);
+    $("#tweets-container").prepend($tempData);
+  });
+};
+
+function loadTweets() {
+  $("#tweets-container").empty(); // empties the container so there is not duplicates
+
+  $.ajax({
+    url: "/tweets",
+    method: "GET",
+    dataType: "json",
+    success: (tweet) => {
+      renderTweets(tweet);
+    },
+  });
+}
+
+const createTweetElement = function (tweet) {
+  const name = tweet.user.name;
+  const avatars = tweet.user.avatars;
+  const handle = tweet.user.handle;
+  // const content = tweet.content.text;
+  const created = moment(tweet.created_at).fromNow();
+  let $tweet = `<section class="kuro">
       <header id="tweetHeader">
       <div id="avatarName">
       <img src="${avatars}"/> 
@@ -95,12 +71,12 @@ loadTweets();
       </div> 
       </footer>
       </section>`;
-    return $tweet;
-  }
-  // ${content}${escape(tweet.content.text)}
+  return $tweet;
+};
 
-  const escape = function (str) {
-    let div = document.createElement("div");
-    div.appendChild(document.createTextNode(str));
-    return div.innerHTML;
-  };
+/// Prevent Cross Scripting
+const escape = function (str) {
+  let div = document.createElement("div");
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+};
